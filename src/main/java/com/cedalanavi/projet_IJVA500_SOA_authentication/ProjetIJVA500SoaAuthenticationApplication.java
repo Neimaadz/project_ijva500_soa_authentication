530e13565255de7a0217597cfbf0ae12a029fb8a
Neimaad <damienlaoussing@hotmail.com>
2022-12-04 15:09:58 +0100

    first commit

---
+++ src/main/java/com/cedalanavi/projet_IJVA500_SOA_authentication/ProjetIJVA500SoaAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.cedalanavi.projet_IJVA500_SOA_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetIJVA500SoaAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetIJVA500SoaAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cedalanavi/projet_IJVA500_SOA_authentication/ProjetIJVA500SoaAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cedalanavi.projet_IJVA500_SOA_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetIJVA500SoaAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
